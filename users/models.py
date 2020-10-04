2b0451c3772d91f8cc0e932bab014a9a47a99775
Roberto Capurro <recapurro327@gmail.com>
2020-10-04 18:58:11 -0500

    custom user created

---
+++ users/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+import uuid
+
+# Create your models here.
+class CustomUser(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(blank=True, max_length=255)
+
+    def __str__(self):
+        return self.email
